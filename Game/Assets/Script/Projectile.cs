ff17352b583f56ec726002f6c9a226b805c5c601
j-pats <jessetpats@gmail.com>
2023-10-24 17:27:01 -0600

    Added to projectile: death effect, lifetime, sprite animation that rotates to face direction of travel
    Added projectile death effect prefab
    Modified projectile prefab to detach trail renderer on de-spawn so the trail renderer doesn't instantly disappear
    Created DestroyAfterTime script to destroy any game object after a given time
    Projectile script uses Coroutine to despawn.
    Projectile script now has deatheffect and trail gameobjects
     Modified Hit-3 animation used in projectile deatheffect to only play once

--- Game/Assets/Script/Projectile.cs
+++ Game/Assets/Script/Projectile.cs
@@ -6,19 +6,41 @@ public class Projectile : MonoBehaviour
 {
 
     public Rigidbody2D rb; //projectiles rigidbody
+    public GameObject deathEffect; // Effect on projectile death
+    public GameObject trail; //Trail child object
 
     public float baseSpeed = 2.0f;
+    public float baseLifetime = 3.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(DespawnCoroutine(baseLifetime));
+    }
+
+    IEnumerator DespawnCoroutine(float waitTime)
+    {
+        // wait for specified time
+        yield return new WaitForSeconds(waitTime);
+        Despawn();
+        yield return null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Rotate sprite to face direction of travel
+        Vector2 rotation = rb.velocity.normalized;
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rotZ);
+    }
+
+    private void Despawn()
+    {
+        Instantiate(deathEffect,transform.position, transform.rotation);
+        trail.transform.parent = null;
+        trail.GetComponent<TrailRenderer>().autodestruct = true;
+        Destroy(gameObject);
     }
 
     /** Fire()
