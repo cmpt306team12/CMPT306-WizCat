d9c12dfd9bb701a10d9c6e418b0eee8611f77c09
AndyMandyCandy <andyanan124@gmail.com>
2023-10-27 18:43:50 -0600

    added in shooting (kinda???)

--- Game/Assets/Script/Wand.cs
+++ Game/Assets/Script/Wand.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Wand : MonoBehaviour
-{
+{ 
+    [SerializeField] private GameObject projectile;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,14 @@ public class Wand : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            projectile.GetComponent<Projectile>().Fire();
+            Instantiate(projectile, transform.position, transform.rotation);
+            
+        }
+
+
     }
 }
