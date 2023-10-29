3c9bd358d88b770fac08f6402f106554f803f275
JL M <jldmeshow@gmail.com>
2023-10-29 17:12:51 -0600

    Add basic placeholder enemy that moves and shoots
    - Enemy will shoot at the center of the player hitbox withing +/-5
      degrees
    - Enemy will move around randomly
    - Also fixed a bug where player was not interacting with breakable
      objects

--- Game/Assets/Script/Enemy.cs
+++ Game/Assets/Script/Enemy.cs
@@ -1,18 +1,58 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private Wand wand;
+
+    [SerializeField] private float firingCooldown;
+    [SerializeField] private float moveSpeed;
+
+    private bool _canFire = true;
+    private bool _canChangeDirection = true;
+
+    private Vector3 _moveDir = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    private void FixedUpdate()
+    {
+        transform.Translate(_moveDir * moveSpeed);
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (_canChangeDirection)
+        {
+            StartCoroutine(ChangeDirection());
+        }
+        if (_canFire)
+        {
+            StartCoroutine(FireProjectile());
+        }
+    }
+
+    private IEnumerator ChangeDirection()
+    {
+        _canChangeDirection = false;
+        float movementAngle = Random.Range(0f, 360f);
+        _moveDir = Quaternion.AngleAxis(movementAngle,Vector3.forward) * Vector3.up;
+        yield return new WaitForSeconds(2);
+        _canChangeDirection = true;
+    }
+    private IEnumerator FireProjectile()
+    {
+        _canFire = false;
+        wand.FireProjectile(GameManager.instance.GetPlayer().transform.position ,transform.position);
+        yield return new WaitForSeconds(firingCooldown);
+        _canFire = true;
     }
 }
--- Game/Assets/Script/Wand.cs
+++ Game/Assets/Script/Wand.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Wand : MonoBehaviour
 {
+    [SerializeField] private Projectile projectilePrefab;
+    
+    private static readonly Vector3 AimingOffset = new Vector3(0, 0.475f, 0);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,19 @@ public class Wand : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void FireProjectile(Vector3 targetPosition, Vector3 startingPosition)
+    {
+        Vector3 offsetTargetPosition = targetPosition + AimingOffset;
+        Vector3 firingDirection = (offsetTargetPosition - startingPosition).normalized;
+        float firingAngle = Mathf.Atan2(firingDirection.y, firingDirection.x) * Mathf.Rad2Deg;
+        float randomOffsetAngle = Random.Range(-5f, 5f);
+        
+        Projectile projectile = Instantiate(
+            projectilePrefab,
+            firingDirection + startingPosition,
+            Quaternion.Euler(0,0,firingAngle + randomOffsetAngle));
+        projectile.Fire();
     }
 }
