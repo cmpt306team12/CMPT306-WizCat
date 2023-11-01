a19cf447d7bfa74636cc6b4400b8c89999b8ce10
j-pats <jessetpats@gmail.com>
2023-11-01 13:50:28 -0600

    Added ProjectileProperties Script and Modified Projectile Script to use Projectile properties
    Moved all projectile properties into separate script: ProjectileProperties
    Every projectile created now needs to be assigned a ProjectileProperties reference before calling fire()
    Projectiles prefab now has a ProjectileProperties attribute that is set when firing - Makes it so that when the player fires a projectile it gets the players projectile properties while an enemies projectile will get the enemies projectile properties.

--- Game/Assets/Script/Projectile.cs
+++ Game/Assets/Script/Projectile.cs
@@ -1,29 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SubsystemsImplementation;
+using UnityEngine.UIElements;
 
 public class Projectile : MonoBehaviour
 {
+
+    public ProjectileProperties ProjectileProperties;
 
     public Rigidbody2D rb; //projectiles rigidbody
     public GameObject deathEffect; // Effect on projectile death
     public GameObject trail; //Trail child object
 
-    public float baseSpeed = 2.0f;
-    public float baseLifetime = 3.0f;
-    public int bounces = 1;
+    // Local variables needed for applying perks over projectile lifetime
+    private bool despawning = false;
+    private int bouncesLeft;
 
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(DespawnCoroutine(baseLifetime));
+        
     }
 
     IEnumerator DespawnCoroutine(float waitTime)
     {
         // wait for specified time
         yield return new WaitForSeconds(waitTime);
-        Despawn();
+        Despawn("Coroutine");
         yield return null;
     }
 
@@ -36,9 +41,13 @@ public class Projectile : MonoBehaviour
         transform.rotation = Quaternion.Euler(0, 0, rotZ);
     }
 
-    public void Despawn()
+    public void Despawn(string from)
     {
-        Instantiate(deathEffect,transform.position, transform.rotation);
+        despawning = true;
+        Debug.Log(from);
+        GameObject effect = Instantiate(deathEffect,transform.position, transform.rotation);
+        float scale = ProjectileProperties.getScale();
+        effect.transform.localScale = new Vector3(scale, scale, scale);
         trail.transform.parent = null;
         trail.GetComponent<TrailRenderer>().autodestruct = true;
         Destroy(gameObject);
@@ -47,24 +56,38 @@ public class Projectile : MonoBehaviour
     /** Fire()
      * Applies the impulse to fire the projectile, depends on the projectiles stats.
      * */
-    public void Fire()
+    public void Fire(ProjectileProperties prop)
     {
-        rb.velocity = transform.right * baseSpeed;
+        this.ProjectileProperties = prop;
+        rb.velocity = transform.right * prop.getSpeed();
+        StartCoroutine(DespawnCoroutine(prop.getLifetime()));
+        ApplyScale(prop.getScale());
+        bouncesLeft = prop.getBounces();
     }
 
+    private void ApplyScale(float scale)
+    {
+        Debug.Log("Scale: " + scale);
+        gameObject.transform.localScale = new Vector3(scale, scale, scale);
+    }
+
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Projectile"))
+        
+        if (collision.CompareTag("Projectile") && !despawning)
         {
-            collision.gameObject.GetComponent<Projectile>().Despawn();
-            Despawn();
+            Despawn("Trigger Proj");
         } else if (collision.CompareTag("Wall") || collision.CompareTag("Obstacle"))
         {
-            if (bounces == 0)
+            Debug.Log("Collision");
+            this.bouncesLeft = this.bouncesLeft - 1;
+            if (this.bouncesLeft < 0)
             {
-                Despawn();
+                Despawn("Trigger Wall/Obs");
             }
-            bounces--;
+            
+            
         }
     }
 }
---
+++ Game/Assets/Script/ProjectileProperties.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileProperties : MonoBehaviour
+{
+    // Base projectile stats used to determine perk modified stats
+    private static float baseSpeed = 8.0f;
+    private static float baseLifetime = 3.0f;
+    private static int baseBounces = 0;
+    private static float baseDamage = 5.0f;
+    private static float baseScale = 1.0f;
+    [SerializeField] float speedFactor = 1.5f;
+    [SerializeField] float lifetimeMod = 0.5f;
+    [SerializeField] float damageMod = 3.0f;
+    [SerializeField] float scaleMod = 1.25f;
+
+
+    // Projectile stats 
+    private int bounces;
+    private float speed;
+    private float lifetime;
+    private float damage;
+    private bool explosive;
+    private float scale;
+
+    private void Start()
+    {
+        // Set all stats to base values
+        bounces = 0;
+        speed = baseSpeed;
+        lifetime = baseLifetime;
+        damage = baseDamage;
+        explosive = false;
+        scale = baseScale;
+    }
+
+    public void ApplyPerks(int[] perks)
+    {
+        for (int i = 0; i < perks.Length; i++)
+        {
+            switch (i)
+            {
+                case 0: // Bounce Perk: +1 bounce per bounce perk
+                    this.bounces = baseBounces + perks[i];
+                    break;
+
+                case 1: // Speed up/down perks: +50% per speed perk
+                    this.speed = baseSpeed * (Mathf.Pow(speedFactor, perks[i]));
+                    Debug.Log(this.speed);
+                    break;
+
+                case 2: // Lifetime up/down perks
+                    this.lifetime = baseLifetime + (perks[i] * lifetimeMod);
+                    break;
+
+                case 3: // Explosive
+                    this.explosive = perks[i] > 0;
+                    break;
+
+                case 4: // Damage up/down
+                    this.damage = baseDamage + (perks[i] * damageMod);
+                    break;
+
+                case 5: // Size up/down
+                    this.scale = baseScale * (Mathf.Pow(scaleMod, perks[i]));
+                    break;
+
+                default:
+                    Debug.LogError("Applying undefined PerkID: " + i);
+
+                    break;
+            }
+        }
+    }
+
+    public float getLifetime() { return lifetime; }
+    public float getDamage() { return damage; }
+    public float getSpeed() { return speed; }
+    public int getBounces() { return bounces; }
+    public bool isExplosive() {  return explosive; }
+    public float getScale() {  return scale; }
+}
