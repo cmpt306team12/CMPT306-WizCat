c60f8f609dadf63a12f5361c4a65d7a747435895
Christophe Gerard <113386764+Flawlessgod@users.noreply.github.com>
2023-11-07 13:10:21 -0600

    add cursor follow

--- Game/Assets/Script/CameraFollow.cs
+++ Game/Assets/Script/CameraFollow.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    public Transform player;
+    public float cameraSize = 5.0f;
+    public float smoothing = 100.0f;
 
-    public Transform target;
-    public Vector3 offset;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        Camera.main.orthographicSize = cameraSize;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (target != null)
-        {
-            transform.position = new Vector3(target.position.x + offset.x, target.position.y + offset.y, offset.z);
-        }
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 playerPosition = player.position;
+        Vector3 targetPosition = new Vector3(
+            (mousePosition.x + playerPosition.x) / 2.4f,
+            (mousePosition.y + playerPosition.y) / 2.4f,
+            transform.position.z
+        );
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime); // smooth
     }
 }
