3f3c0f249068521b7cf5eb589a48a0866949f888
AndyMandyCandy <andyanan124@gmail.com>
2023-11-13 22:14:12 -0600

    Implemented 4 directional animation

--- Game/Assets/Script/PlayerMovement.cs
+++ Game/Assets/Script/PlayerMovement.cs
@@ -17,6 +17,7 @@ public class PlayerMovement : MonoBehaviour
     public float dashDrag = 1.0f; 
     // public GameObject dashEffect;
     public bool canDash = false;
+    public bool faceUp = false;
 
     public float movementSpeed = 5;
     public Animator animator;
@@ -26,34 +27,54 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         dashTime = startDashTime;
         animator = GetComponent<Animator>();
-    }
+   
+    } 
+
 
     void Update()
     {
+
+        //---Strictly Player Movement Controls------------------------------------------------------------------------------------------------------------------------------
         Vector2 dir = Vector2.zero;
         if (Input.GetKey(KeyCode.A))
         {
+            if (faceUp == false)
+            {
+                animator.Play("CatWizSideIdleL");
+            }
+            else
+            {
+                animator.Play("CatWizBackSideIdleL");
+            }
             dir.x = -1;
-            animator.Play("CatWizSideIdleL");
+
         }
         else if (Input.GetKey(KeyCode.D))
         {
+            if (faceUp == false)
+            {
+                animator.Play("CatWizSideIdleR");
+            }
+            else
+            {
+                animator.Play("CatWizBackSideIdleR");
+            }
             dir.x = 1;
-            animator.Play("CatWizSideIdleR");
+
         }
 
         if (Input.GetKey(KeyCode.W))
         {
+            faceUp = true;
             dir.y = 1;
         }
         else if (Input.GetKey(KeyCode.S))
         {
+            faceUp = false;
             dir.y = -1;
         }
-
-        // dir.Normalize();
-        // GetComponent<Rigidbody2D>().velocity = movementSpeed * dir;
-
+        
+        //---End of Player Movement Controls------------------------------------------------------------------------------------------------------------------------------
 
         // Dash on spacebar down
         if (Input.GetKeyDown(KeyCode.Space) && dashTime <= 0 && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D)))
@@ -61,40 +82,44 @@ public class PlayerMovement : MonoBehaviour
             catSFX.PlayOneShot(dashSFX);
             dashDirection = dir.normalized;
             dashTime = startDashTime;
-            rb.velocity = Vector2.zero; 
+            rb.velocity = Vector2.zero;
             rb.AddForce(dashDirection * dashImpulseForce, ForceMode2D.Impulse);
             // Instantiate(dashEffect, transform.position, Quaternion.identity);
 
         }
-        if (canDash==true){
+        if (canDash == true)
+        {
             if (dashTime > 0)
-                    {
-                        dashTime -= Time.deltaTime;
-                    }
-                    else
-                    {
-                        rb.velocity = rb.velocity * (1 - dashDrag * Time.deltaTime);
-                        if (dir == Vector2.zero)
-                        {
-                            rb.velocity = Vector2.zero;
-                        }
+            {
+                dashTime -= Time.deltaTime;
+            }
+            else
+            {
+                rb.velocity = rb.velocity * (1 - dashDrag * Time.deltaTime);
+                if (dir == Vector2.zero)
+                {
+                    rb.velocity = Vector2.zero;
+                }
 
-                        // If not dashing use walk speed
-                        if (dashTime <= 0 && dir != Vector2.zero){
-                            dir.Normalize();
-                            rb.velocity = movementSpeed * dir;
-                        }
-                    }
+                // If not dashing use walk speed
+                if (dashTime <= 0 && dir != Vector2.zero)
+                {
+                    dir.Normalize();
+                    rb.velocity = movementSpeed * dir;
+                }
+            }
 
         }
 
         // Can't dash
-        else {
+        else
+        {
             dir.Normalize();
             rb.velocity = movementSpeed * dir;
         }
-    
-    
+
+
+
 
     }
 }
