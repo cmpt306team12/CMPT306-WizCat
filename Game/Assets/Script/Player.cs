a70e71bf180145b69697a662d4937d770f8365d4
j-pats <jessetpats@gmail.com>
2023-12-05 11:16:39 -0600

    Added Perk Achievement Text
    perk found text when collecting Perk for the first time
    WizCat prefab modified to show text
    Player script modified to show text

--- Game/Assets/Script/Player.cs
+++ Game/Assets/Script/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using static UnityEngine.UIElements.UxmlAttributeDescription;
 
@@ -22,6 +23,9 @@ public class Player : MonoBehaviour
     // Fire rate stuff
     public float cooldown = 0.5f;
     public bool canFire = true;
+
+    // Text for perk achievement
+    public GameObject AccessFloatingTextPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -93,7 +97,42 @@ public class Player : MonoBehaviour
             gameObject.GetComponent<Perks>().AddPerk(id);
             GetComponentInChildren<AudioSource>().clip = perkSound;
             StartCoroutine(softerPerk());
+            // Need to get perk achievement
+            if (PlayerPrefs.GetInt("Collectedperk" + id, 0) == 0)
+            {
+                PlayerPrefs.SetInt("Collectedperk" + id, 1);
+                PlayerPrefs.Save();
+                StartCoroutine(PerkFound(id));
+            }
         }
+    }
+
+    IEnumerator PerkFound(int perkid)
+    {
+        string[] perkNames = {"Bounce", "Speed+", "Speed-", "Lifetime+", "Lifetime-", "Damage+","Damage-", "Explosive",
+            "Size+", "Size-", "Burst", "Bite", "Dash", "Orbit", "Split", "Homing", "Boomerang", "Wiggle", "Health+"};
+        // Display reward of access and stop particle effect
+        Vector3 offset = new Vector3(0.0f, -1.0f, 0.0f);
+        GameObject text = Instantiate(AccessFloatingTextPrefab, gameObject.transform.position + offset, Quaternion.identity);
+        TextMeshProUGUI textMesh = text.GetComponentInChildren<TextMeshProUGUI>();
+        string Text = perkNames[perkid] + " found!";
+        textMesh.text = Text;
+        Destroy(text, 3.0f);
+        float time = 0.0f;
+        float duration = 2.0f;
+        float riseSpeed = 0.5f;
+        Vector3 originalPosition = textMesh.transform.position;
+
+        while (time < duration)
+        {
+            float alpha = Mathf.PingPong(time * 2.0f, 1.0f);
+            textMesh.color = new Color(textMesh.color.r, textMesh.color.g, textMesh.color.b, alpha);
+            textMesh.transform.position += Vector3.up * riseSpeed * Time.deltaTime;
+            time += Time.deltaTime;
+            yield return null;
+        }
+        textMesh.color = new Color(textMesh.color.r, textMesh.color.g, textMesh.color.b, 0.0f);
+        textMesh.transform.position = originalPosition;
     }
 
     IEnumerator softerPerk()
